f1b18c3ef2ead090b80cdca90f8eb69ecde68cbe
Gerroar <germanariasrodriguez@gmail.com>
2023-08-08 13:41:07 +0200

    Animals onm index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-const encoder = new TextEncoder()
+const encoder = new TextEncoder("utf-8");
+const decoder = new TextDecoder("utf-8");
 
 export async function handleRequest(request) {
+  const animalFact = await fetch(
+    "https://random-data-api.fermyon.app/animals/json"
+  );
 
-    return {
-        status: 200,
-        headers: { "foo": "bar" },
-        body: encoder.encode("Hello from JS-SDK").buffer
-    }
+  const animalFactBody = decoder.decode(
+    (await animalFact.arrayBuffer()) || new Uint8Array()
+  );
+
+  const env = JSON.stringify(process.env);
+
+  const body = `Here's an animal fact: ${animalFactBody}\n`;
+  return {
+    status: 200,
+    headers: { foo: "bar" },
+    body: encoder.encode(body).buffer,
+  };
 }
